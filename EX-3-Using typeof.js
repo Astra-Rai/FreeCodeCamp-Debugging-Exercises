d01ddb28dac5dd37f435c5f900de57806a8ae042
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-02-14 10:39:10 -0600

    Using typeof operator, immutable data types

---
+++ EX-3-Using typeof.js
@@ -0,0 +1,20 @@
+//Instructions
+/*
+
+Your code should use typeof in two console.log() statements to check the type of the variables.
+
+Your code should use typeof to check the type of the variable seven.
+
+Your code should use typeof to check the type of the variable three.
+*/
+
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Add your code below this line
+console.log(typeof seven);//-> number
+console.log(typeof three);//->string
+
+//immutable data types in Javascript
+//Boolean, Null, Undefined, Number String, Symbol
+//Mutable: Object
