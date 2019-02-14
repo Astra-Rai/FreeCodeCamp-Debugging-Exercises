5d5a2cd901a2c6a3f65d927b1e60ba0bea2ad632
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-02-14 10:28:31 -0600

    Check Var Value Using console.log()

---
+++ EX-1-Check-Var-Value.js
@@ -0,0 +1,10 @@
+//Instructions
+//Use the console.log() method to print the value of the variable a where noted in the code.
+
+let a = 5;
+let b = 1;
+a++;
+// Add your code below this line
+
+let sumAB = a + b;
+console.log(a);//->6
