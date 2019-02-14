fe95c3875fb6bc3c4d5be490eb2721f8abcb65a6
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-02-14 10:47:46 -0600

    Check Misspellings of Variable and Functions

---
+++ EX-4-Check Misspellings.js
@@ -0,0 +1,20 @@
+//Instructions
+/*Fix the two spelling errors in the code so the netWorkingCapital calculation works
+
+//Before
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = recievables - payable;
+console.log(`Net working capital is: ${netWorkingCapital}`);
+
+*/
+
+//After
+
+let receivables = 10;//
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+//corrected spelling of receivable, letters "e and i" transposed
+//added 's' to word payable
+//corrected spelling of networking
+console.log(`Networking capital is: ${netWorkingCapital}`);//->"Networking capital is: 2"
